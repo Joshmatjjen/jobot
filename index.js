aa3f7684063a6bfd21824a7dab8f8f7186178c21
Joshmatjjen <joshmatparrot@gmail.com>
2019-12-18 09:50:44 +0100

    end of chatbot(new)

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'production') {
   // js and css files
   app.use(express.static('client/build'));
 
-  // index.html for all pages routes
+  // index.html for all page routes
   const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
