99b3cb5aacb59f4e5d0faf120fa4c202060c66d6
Joshmatjjen <joshmatparrot@gmail.com>
2019-12-18 21:48:15 +0100

    prepare to deploying

--- routes/fulfillmentRoutes.js
+++ routes/fulfillmentRoutes.js
@@ -2,7 +2,6 @@ const {WebhookClient} = require('dialogflow-fulfillment');
 
 const mongoose = require('mongoose');
 const Demand = mongoose.model('demand');
-const Coupon = mongoose.model('coupon');
 
 module.exports = app => {
   app.post('/', async (req, res) => {
@@ -15,45 +14,7 @@ module.exports = app => {
       agent.add(`Welcome to my Parrot fulfillment!`);
     }
 
-    async function registration(agent) {
-      const registration = new Registration({
-        name: agent.parameters.name,
-        address: agent.parameters.address,
-        phone: agent.parameters.phone,
-        email: agent.parameters.email,
-        dateSent: Date.now(),
-      });
-      try {
-        let reg = await registration.save();
-        console.log(reg);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-
-    // async function learn(agent) {
-    //   Demand.findOne({course: agent.parameters.courses}, function(err, course) {
-    //     if (course !== null) {
-    //       course.counter++;
-    //       course.save();
-    //     } else {
-    //       const demand = new Demand({course: agent.parameters.courses});
-    //       demand.save();
-    //     }
-    //   });
-    //   let responseText = `You want to learn about ${agent.parameters.courses}.
-    //                 Here is a link to all of my courses: https://www.facebook.com/JoshmatJjen`;
-
-    //   let coupon = await Coupon.findOne({course: agent.parameters.courses});
-    //   if (coupon !== null) {
-    //     responseText = `You want to learn about ${agent.parameters.courses}.
-    //             Here is a link to the course: ${coupon.link}`;
-    //   }
-
-    //   agent.add(responseText);
-    // }
-
-    async function learn(agent) {
+    function learn(agent) {
       Demand.findOne({course: agent.parameters.courses}, function(err, course) {
         if (course !== null) {
           course.counter++;
@@ -62,25 +23,21 @@ module.exports = app => {
           const demand = new Demand({
             course: agent.parameters.courses,
           });
-          if (demand.course !== '') {
-            demand.save();
-          }
+          console.log(demand);
+          console.log(demand.course);
+          demand.save();
         }
       });
       let responseText = `
-        You want to learn about ${agent.parameters.courses}.
+        You want to learn about ${agent.parameters.courses}. 
         Here is a link to all of my courses: https://jclothing.herokuapp.com
       `;
-      // let badResponseText = ` 😢 Sorry this course is not available, please try other courses `;
-      // if (agent.parameters.courses === '') {
-      //   agent.add(badResponseText);
-      // } else {
-      let coupon = await Coupon.findOne({course: agent.parameters.courses});
-      if (coupon !== null) {
-        responseText = `You want to learn about ${agent.parameters.courses}. Here is a link to the course: ${coupon.link}`;
+      let badResponseText = ` 😢 Sorry this course is not available, please try other courses `;
+      if (agent.parameters.course === '') {
+        agent.add(badResponseText);
+      } else {
+        agent.add(responseText);
       }
-      agent.add(responseText);
-      // }
     }
 
     function fallback(agent) {
@@ -90,7 +47,7 @@ module.exports = app => {
     let intentMap = new Map();
     intentMap.set('parrot', parrot);
     intentMap.set('learn courses', learn);
-    intentMap.set('recommend courses - yes', registration);
+
     intentMap.set('Default Fallback Intent', fallback);
     agent.handleRequest(intentMap);
   });
