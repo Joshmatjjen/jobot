9fc9018dbba0466dff9183c082e1eaf10a0b42ac
Joshmatjjen <joshmatparrot@gmail.com>
2019-12-18 11:47:45 +0100

    prepare to deploy

--- index.js
+++ index.js
@@ -25,16 +25,16 @@ require('./routes/fulfillmentRoutes')(app);
 
 // mongoose.connect(config.mongoURI, {useNewUrlParser: true});
 
-// if (process.env.NODE_ENV === 'production') {
-//   // js and css files
-//   app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  // js and css files
+  app.use(express.static('client/build'));
 
-//   // index.html for all page routes
-//   const path = require('path');
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
+  // index.html for all page routes
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
