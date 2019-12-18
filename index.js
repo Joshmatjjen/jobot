3500c55f37686f859234abaf3ca7f0b0eb8db58c
Joshmatjjen <joshmatparrot@gmail.com>
2019-12-18 12:37:56 +0100

    prepare to deploy

--- index.js
+++ index.js
@@ -16,25 +16,25 @@ mongoose
   .catch(err => console.log(err.message.reason));
 app.use(bodyParser.json());
 
-// mongoose.connect(config.mongoURI, {useNewUrlParser: true});
-console.log('You are in ', process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'production') {
-  // js and css files
-  app.use(express.static('/client/build'));
-
-  // index.html for all page routes
-  const path = require('path');
-  app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
-
 require('./models/Registration');
 require('./models/Demand');
 require('./models/Coupons');
 
 require('./routes/dialogFlowRoutes')(app);
 require('./routes/fulfillmentRoutes')(app);
+
+// mongoose.connect(config.mongoURI, {useNewUrlParser: true});
+console.log('You are in ', process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'production') {
+  // js and css files
+  app.use(express.static('client/build'));
+
+  // index.html for all page routes
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
