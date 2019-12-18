66897b34ae94a09b833fcaa85bbffbf0f56e733d
Joshmatjjen <joshmatparrot@gmail.com>
2019-12-18 12:46:41 +0100

    prepare to deploy

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
 
 const config = require('./config/keys');
-
 const mongoose = require('mongoose');
-mongoose
-  .connect(config.mongoURI, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => console.log('DB connection successful!😊'))
-  .catch(err => console.log(err.message.reason));
-app.use(bodyParser.json());
+mongoose.connect(config.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 require('./models/Registration');
 require('./models/Demand');
 require('./models/Coupons');
 
+app.use(bodyParser.json());
+
 require('./routes/dialogFlowRoutes')(app);
 require('./routes/fulfillmentRoutes')(app);
 
-// mongoose.connect(config.mongoURI, {useNewUrlParser: true});
-console.log('You are in ', process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'production') {
   // js and css files
   app.use(express.static('client/build'));
@@ -32,10 +23,9 @@ if (process.env.NODE_ENV === 'production') {
   // index.html for all page routes
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
