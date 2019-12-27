e3750002dfb03f8879f3064f4e959fec0ff105a4
Joshmatjjen <joshmatparrot@gmail.com>
2019-12-27 01:28:09 +0100

    First Redesign v1.3

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Chatbot from '../chatbot/Chatbot';
 const About = () => {
   return (
-    <div>
+    <div style={{textAlign: 'center', height: '100%', width: '100%'}}>
       <h1>About us!</h1>
       <Chatbot />
     </div>
--- client/src/components/shop/Shop.js
+++ client/src/components/shop/Shop.js
@@ -3,7 +3,7 @@ import ItemList from './ItemList';
 import Chatbot from '../chatbot/Chatbot';
 
 const Shop = () => (
-  <div>
+  <div style={{textAlign: 'center', height: '100%', width: '100%'}}>
     <h2>Shop</h2>
     <p>I am the best</p>
     <ItemList />
